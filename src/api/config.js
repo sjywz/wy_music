7e40f870b20362cd8dc0bf5f89844e2a5110927e
柳周侯爷 <sjywz@foxmail.com>
2022-12-16 14:14:00 +0800

    请求数据

---
+++ src/api/config.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://cloudapi.bcode.cc/';
+
+//axios 的实例及拦截器配置
+const axiosInstance = axios.create ({
+  baseURL: baseUrl
+});
+
+axiosInstance.interceptors.response.use (
+  res => res.data,
+  err => {
+    console.log (err, "网络错误");
+  }
+);
+
+axiosInstance.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+export {
+  axiosInstance
+};
