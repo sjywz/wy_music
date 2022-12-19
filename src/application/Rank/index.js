0c4ab1a720d4d559b828d1a69a4ba6f840726e50
柳周侯爷 <sjywz@foxmail.com>
2022-12-19 17:41:03 +0800

    排行榜

--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,5 +1,95 @@
-const Index = () => {
-	return <div>rank</div>
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getRankList } from './store/index';
+import { filterIndex } from '@/api/utils';
+import Scroll from '@/components/scroll';
+import { Container, List, ListItem, SongList, EnterLoading } from './style';
+import Loading from '@/baseUI/loading';
+import { renderRoutes } from 'react-router-config'
+
+const renderRankList = (list, global) => {
+	const enterDetail = () => {
+
+	}
+	return (
+		<List globalRank={global}>
+			{
+				list.map((item) => {
+					return (
+						<ListItem 
+							key={item.id} 
+							tracks={item.tracks} 
+							onClick={() => enterDetail(item.name)}
+						>
+							<div className="img_wrapper">
+								<img src={item.coverImgUrl} alt="" />
+								<div className="decorate"></div>
+								<span className="update_frequecy">{item.updateFrequency}</span>
+							</div>
+							{renderSongList(item.tracks)}
+						</ListItem>
+					)
+				})
+			}
+		</List>
+	)
 }
 
-export default Index;
+const renderSongList = (list) => {
+	return list.length ? (
+		<SongList>
+			{
+				list.map((item, index) => {
+					return <li key={index}>{index + 1}. {item.first} - {item.second}</li>
+				})
+			}
+		</SongList>
+	) : null;
+}
+
+const Index = (props) => {
+	const { rankList: list, loading } = props;
+
+	const { getRankListDataDispatch } = props;
+	let rankList = list ? list.toJS() : [];
+
+	let globalStartIndex = filterIndex(rankList);
+	let officialList = rankList.slice(0, globalStartIndex);
+	let globalList = rankList.slice(globalStartIndex);
+
+	useEffect(() => {
+		if(!list.size) {
+			getRankListDataDispatch();
+		}
+	}, []);
+
+	let displayStyle = loading ? { "display": "none" } : { "display": "" };
+
+	return <Container>
+		<Scroll>
+			<div>
+				<h1 className="offical" style={displayStyle}> 官方榜 </h1>
+				{renderRankList(officialList)}
+				<h1 className="global" style={displayStyle}> 全球榜 </h1>
+				{renderRankList(globalList, true)}
+				{loading ? <EnterLoading><Loading></Loading></EnterLoading> : null}
+			</div>
+		</Scroll>
+		{renderRoutes(props.route.routes)}
+	</Container>
+}
+
+const mapStateToProps = (state) => ({
+	rankList: state.getIn(['rank', 'rankList']),
+	loading: state.getIn(['rank', 'loading']),
+});
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getRankListDataDispatch() {
+			dispatch(getRankList());
+		}
+	}
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Index));
